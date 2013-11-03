53096ea4d74964d82d2cd63ffa43a78c32cbe930
Rodrigo <rodrigo.paiva@onisolucoes.net>
2013-11-02 22:27:44 -0300

    Created ststus update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '315dc47d8d0dc39847feec526931acc56e5c02822a73a0cdadf12a1e3de18aa8c3eaf076a01f24aaa8288b70994f843f5f6dcf77f1f76336261307cb99fe8817'
